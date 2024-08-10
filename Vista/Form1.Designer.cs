3165a9b862c7130a6fd34405a1ef89c0506642c6
Gabriel Martinez <gaabi26092005@gmail.com>
2024-08-09 23:57:17 -0300

    Estructura inicial de la ventana principal lista.

--- Vista/Form1.Designer.cs
+++ Vista/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Vista
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dvgArticulos = new System.Windows.Forms.DataGridView();
+            this.pboImagen = new System.Windows.Forms.PictureBox();
+            this.lblFiltrarNombre = new System.Windows.Forms.Label();
+            this.txtFiltrarNombre = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnVerDetalles = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.ForeColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(493, 251);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // dvgArticulos
+            // 
+            this.dvgArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgArticulos.Location = new System.Drawing.Point(12, 51);
+            this.dvgArticulos.Name = "dvgArticulos";
+            this.dvgArticulos.Size = new System.Drawing.Size(556, 194);
+            this.dvgArticulos.TabIndex = 1;
+            // 
+            // pboImagen
+            // 
+            this.pboImagen.Location = new System.Drawing.Point(574, 51);
+            this.pboImagen.Name = "pboImagen";
+            this.pboImagen.Size = new System.Drawing.Size(207, 194);
+            this.pboImagen.TabIndex = 2;
+            this.pboImagen.TabStop = false;
+            // 
+            // lblFiltrarNombre
+            // 
+            this.lblFiltrarNombre.AutoSize = true;
+            this.lblFiltrarNombre.Location = new System.Drawing.Point(12, 28);
+            this.lblFiltrarNombre.Name = "lblFiltrarNombre";
+            this.lblFiltrarNombre.Size = new System.Drawing.Size(75, 13);
+            this.lblFiltrarNombre.TabIndex = 3;
+            this.lblFiltrarNombre.Text = "Filtrar Nombre:";
+            // 
+            // txtFiltrarNombre
+            // 
+            this.txtFiltrarNombre.Location = new System.Drawing.Point(87, 25);
+            this.txtFiltrarNombre.Name = "txtFiltrarNombre";
+            this.txtFiltrarNombre.Size = new System.Drawing.Size(167, 20);
+            this.txtFiltrarNombre.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(447, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btnVerDetalles
+            // 
+            this.btnVerDetalles.Location = new System.Drawing.Point(250, 251);
+            this.btnVerDetalles.Name = "btnVerDetalles";
+            this.btnVerDetalles.Size = new System.Drawing.Size(75, 23);
+            this.btnVerDetalles.TabIndex = 6;
+            this.btnVerDetalles.Text = "Ver Detalles";
+            this.btnVerDetalles.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(331, 251);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(412, 251);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 8;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(320, 24);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(793, 293);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnVerDetalles);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtFiltrarNombre);
+            this.Controls.Add(this.lblFiltrarNombre);
+            this.Controls.Add(this.pboImagen);
+            this.Controls.Add(this.dvgArticulos);
+            this.Controls.Add(this.btnEliminar);
+            this.Name = "Principal";
+            this.Text = "Articulos";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dvgArticulos;
+        private System.Windows.Forms.PictureBox pboImagen;
+        private System.Windows.Forms.Label lblFiltrarNombre;
+        private System.Windows.Forms.TextBox txtFiltrarNombre;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnVerDetalles;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
